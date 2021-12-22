0bddd414edacb48eae972fd723d04fe62fe94411
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2021-12-22 23:17:32 +0200

    task__2--fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,14 +7,12 @@ const player = new Player(iframe);
 const fn = e => {
   let timeCode = `${e.seconds}`;
   localStorage.setItem('videoplayer-current-time', timeCode);
-  console.log(timeCode);
 };
 
 player.on('timeupdate', throttle(fn, 1000));
 
 if (localStorage.getItem('videoplayer-current-time')) {
   const time = localStorage.getItem('videoplayer-current-time');
-  console.log('Локальное чтение', time);
 
   player.setCurrentTime(time);
 }
