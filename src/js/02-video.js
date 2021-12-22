63148d53b45efafb088c37b9958b785de1d010e1
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2021-12-22 22:52:08 +0200

    add task__2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const fn = e => localStorage.setItem('videoplayer-current-time', e.seconds);
+
+player.on('timeupdate', throttle(fn, 1000));
+
+if (localStorage.getItem('videoplayer-current-time')) {
+  const time = localStorage.getItem('videoplayer-current-time');
+
+  player.setCurrentTime(time);
+}
