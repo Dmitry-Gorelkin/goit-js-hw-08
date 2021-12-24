9649016adcc8b6434d4929b30e6d6e82c69720ed
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2021-12-25 00:05:47 +0200

    task__3--megabagfix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,20 +1,13 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
 const refs = {
-  form: document.querySelector(".feedback-form"),
-  email: document.querySelector(".feedback-form input"),
-  message: document.querySelector(".feedback-form textarea"),
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  message: document.querySelector('.feedback-form textarea'),
 };
 
-const LOCAL_KEY = "feedback-form-state";
+const LOCAL_KEY = 'feedback-form-state';
 let objForm = {};
-
-if (localStorage.getItem(LOCAL_KEY)) {
-  transformationJSON();
-
-  refs.email.value = objForm.email ? objForm.email : "";
-  refs.message.value = objForm.message ? objForm.message : "";
-}
 
 function onInput(e) {
   objForm[e.target.name] = e.target.value;
@@ -31,7 +24,7 @@ function formSubmit(e) {
   transformationJSON();
 
   if (!objForm.email || !objForm.message) {
-    alert("Вы не заполнили форму.");
+    alert('Вы не заполнили форму.');
     return;
   }
 
@@ -43,10 +36,20 @@ function formSubmit(e) {
 }
 
 function transformationJSON() {
-  if (!JSON.parse(localStorage.getItem(LOCAL_KEY))) return;
+  if (!localStorage.getItem(LOCAL_KEY)) return;
 
   objForm = JSON.parse(localStorage.getItem(LOCAL_KEY));
 }
 
-refs.form.addEventListener("input", throttle(onInput, 500));
-refs.form.addEventListener("submit", formSubmit);
+function fillForm() {
+  if (!localStorage.getItem(LOCAL_KEY)) return;
+
+  transformationJSON();
+
+  refs.email.value = objForm.email ? objForm.email : '';
+  refs.message.value = objForm.message ? objForm.message : '';
+}
+
+fillForm();
+refs.form.addEventListener('input', throttle(onInput, 500));
+refs.form.addEventListener('submit', formSubmit);
